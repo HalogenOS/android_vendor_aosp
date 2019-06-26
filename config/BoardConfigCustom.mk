f62c11beb341e68413525bbf09e80dece4c55d0e
LuK1337 <priv.luk@gmail.com>
2019-06-26 16:20:51 +0200

    pa: sepolicy: Add rules for FOD.
    Change-Id: I067ead0c2f60493a974bc220b67d7039acea4823
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- config/BoardConfigCustom.mk
+++ config/BoardConfigCustom.mk
@@ -5,4 +5,11 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include $(CUSTOM_VENDOR_DIR)/config/BoardConfigQcom.mk
 endif
 
+# SEPolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(CUSTOM_VENDOR_DIR)/sepolicy/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    $(CUSTOM_VENDOR_DIRj)/sepolicy/public
+
 include $(CUSTOM_VENDOR_DIR)/config/BoardConfigSoong.mk
