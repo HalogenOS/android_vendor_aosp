ad51b544c595e14674a492dbc8b869fea612eaf3
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-15 16:03:53 +0530

    aosip: fonts: Update fonts to Android 10 and change output dir
    Extracted from https://github.com/AndroidDumps/google_taimen_dump/tree/taimen-user-10-QP1A.190711.020-5800535-release-keys/system/system/product/fonts
    
    Change-Id: I7fd353cb2991caa1cb9b7b5dfed86225db44ef86
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,10 +1,9 @@
 # Fonts
 PRODUCT_COPY_FILES += \
-    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
-    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
-    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
-    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
-    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
-    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
-
-ADDITIONAL_FONTS_FILE := $(CUSTOM_VENDOR_DIR)/fonts/google-sans.xml
+    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
+    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
+    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
+    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
+    $(CUSTOM_VENDOR_DIR)/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
+    $(CUSTOM_VENDOR_DIR)/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
