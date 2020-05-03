293105e586eba6d1d3778c58d0fadcd77c8146e8
Simao Gomes Viana <devel@superboring.dev>
2020-05-03 18:36:13 +0200

    Build PotatoNotes
    Change-Id: I8c86ffd88666030c01e8baa39907072868295a59

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -4,7 +4,8 @@ $(call inherit-product, $(CUSTOM_VENDOR_DIR)/config/common_full.mk)
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME \
-	Lawnfeed
+	Lawnfeed \
+	PotatoNotes
 
 # Include custom LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += $(CUSTOM_VENDOR_DIR)/overlay/dictionaries
