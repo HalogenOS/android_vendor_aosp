f64b67835d4b6c0ed61e7cbd924fddcb44c557c2
Simao Gomes Viana <devel@superboring.dev>
2020-05-14 22:54:37 +0200

    Revert "Build PotatoNotes"
    doesn't work
    
    This reverts commit 293105e586eba6d1d3778c58d0fadcd77c8146e8.
    
    Change-Id: I2319d5dc6ceece55130906e18627b782ff1de602

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -4,8 +4,7 @@ $(call inherit-product, $(CUSTOM_VENDOR_DIR)/config/common_full.mk)
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME \
-	Lawnfeed \
-	PotatoNotes
+	Lawnfeed
 
 # Include custom LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += $(CUSTOM_VENDOR_DIR)/overlay/dictionaries
